ba4def1dd721940508f335c83e7ac05cdc70e617
Geuseppe Lara <geuseppe18@gmail.com>
2025-05-28 14:10:21 -0500

    A002-GL-Arreglando vainas

--- src/main/java/com/example/demo/service/KeyGenerator.java
+++ src/main/java/com/example/demo/service/KeyGenerator.java
@@ -7,7 +7,6 @@ import java.util.Base64;
 
 public class KeyGenerator {
     public static void main(String[] args) {
-        // Genera una clave segura para HS512 (512 bits)
         byte[] keyBytes = Keys.secretKeyFor(SignatureAlgorithm.HS512).getEncoded();
         String base64Key = Base64.getEncoder().encodeToString(keyBytes);
         System.out.println("Nueva clave secreta para HS512 (Base64):");
